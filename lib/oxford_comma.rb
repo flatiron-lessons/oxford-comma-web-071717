e31c24316b839a3ce1763f4839ccac3047d9d81f
James Miele <jnmiele@verizon.net>
2017-07-06 18:59:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(",")
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    [array[0...-1].join(", "), array.last].join(", and ")
+    # creates a range from array[0] to array[-1]
+    # joins this range with a ", ",
+    # then take the last element of the array and joins it with ", and last"
+  end
 end
